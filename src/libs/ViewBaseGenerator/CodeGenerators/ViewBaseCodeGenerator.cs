241606a91df14cdf79c2826fec5a6d32a327bb9e
Konstantin S <3002068+HavenDV@users.noreply.github.com>
2022-08-22 19:17:08 +1000

    fix: Fixed ViewBaseGenerator explicit type bug.

--- src/libs/ViewBaseGenerator/CodeGenerators/ViewBaseCodeGenerator.cs
+++ src/libs/ViewBaseGenerator/CodeGenerators/ViewBaseCodeGenerator.cs
@@ -8,7 +8,9 @@ internal static class ViewBaseCodeGenerator
     {
         var baseClass = !viewBase.BaseClass.Contains('.') && viewBase.Platform.HasValue
             ? ConstructorCodeGenerator.GenerateTypeByPlatform(viewBase.Platform.Value, $"Controls.{viewBase.BaseClass}")
-            : $"{viewBase.BaseClass.WithGlobalPrefix()}<{viewBase.ViewModel.WithGlobalPrefix()}>";
+            : viewBase.BaseClass.Contains("<>")
+                ? $"{viewBase.BaseClass.Replace("<>", string.Empty).WithGlobalPrefix()}<{viewBase.ViewModel.WithGlobalPrefix()}>"
+                : viewBase.BaseClass.WithGlobalPrefix();
 
         var viewModelType = viewBase.ViewModel.WithGlobalPrefix();
         var dependencyProperty = viewBase.Platform.HasValue
